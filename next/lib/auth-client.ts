a8c2fb25768be9eb3c49de6011fd41d186d10473
paul-nguyen96 <paul121596@icloud.com>
2025-11-06 16:24:19 -0600

    remove nextpublic env

--- next/lib/auth-client.ts
+++ next/lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_BASE_URL as string,
+  baseURL: process.env.BETTER_AUTH_URL as string,
 });
 
 export const { signIn, signUp, signOut, useSession } = authClient;
