0745fa9e72d6997d38acdf49a9e0805f2025cf2e
paul-nguyen96 <paul121596@icloud.com>
2025-11-05 19:14:20 -0600

    update env naming

--- next/lib/auth-client.ts
+++ next/lib/auth-client.ts
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
-  baseURL: process.env.BASE_URL,
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
 });
 
 export const { signIn, signUp, signOut, useSession } = createAuthClient();
