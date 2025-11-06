b4ecbdc4d76f3cf9411844f47e5eedd220c553ab
paul-nguyen96 <paul121596@icloud.com>
2025-11-06 15:08:16 -0600

    export auth client

--- next/lib/auth-client.ts
+++ next/lib/auth-client.ts
@@ -2,7 +2,7 @@ import { createAuthClient } from "better-auth/react";
 
 const baseURL = process.env.NEXT_PUBLIC_BASE_URL as string;
 
-const authClient = createAuthClient({
+export const authClient = createAuthClient({
   baseURL,
 });
 
