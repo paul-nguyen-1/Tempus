79610ec7b33bcb6cfe35c83259bbaeceb5d5b2ab
paul-nguyen96 <paul121596@icloud.com>
2025-11-05 19:32:50 -0600

    add trusted origins

--- next/lib/auth-client.ts
+++ next/lib/auth-client.ts
@@ -1,6 +1,9 @@
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  trustedOrigins: [
+    ...(process.env.VERCEL_URL ? [`https://${process.env.VERCEL_URL}`] : []),
+  ],
 });
 
 export const { signIn, signUp, signOut, useSession } = createAuthClient();
