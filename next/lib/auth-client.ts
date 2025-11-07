af8cda72116e446b4d87d23dc128b9ce2e24ac3f
paul-nguyen96 <paul121596@icloud.com>
2025-11-07 13:42:45 -0600

    update

--- next/lib/auth-client.ts
+++ next/lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: "https://tempus-swart.vercel.app/",
+  baseURL: process.env.BETTER_AUTH_URL as string,
 });
 
 export const { signIn, signUp, signOut, useSession } = authClient;
--- next/lib/auth.ts
+++ next/lib/auth.ts
@@ -9,10 +9,7 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
-  baseURL: "https://tempus-swart.vercel.app/",
-  trustedOrigins: [
-    "http://localhost:3000",
-    "https://tempus-swart.vercel.app",
-  ],
+  basePath: "/api/auth",
+  trustedOrigins: ["http://localhost:3000", "https://tempus-swart.vercel.app"],
   secret: process.env.AUTH_SECRET,
 });
